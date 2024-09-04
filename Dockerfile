220197160e7c9ed5273896110b84fa29ed548693
oddsummer56 <oddsummer56@gmail.com>
2024-09-04 11:21:59 +0900

    fin

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
+#BASE 이미지 시작점
 FROM datamario24/isdomi:0.8.3
 
 WORKDIR /code
 
 COPY src/fishmlserv/main.py /code/
 
+# 모델서빙만(의존성은 위 BASE 이미지에서 모두 설치했다)
 RUN pip install --no-cache-dir --upgrade git+https://github.com/oddsummer56/fishmlserv.git@1.0.0/k
 
+# 모델 서빙을 위해 API 구동을 위한 FastAPI RUN
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
--- src/fishmlserv/model/manager.py
+++ src/fishmlserv/model/manager.py
@@ -2,7 +2,6 @@ import os
 import pickle
 
 def get_model_path():
-    
     path = __file__
 
     # 현재 파일의 디렉토리 경로를 얻음
@@ -12,20 +11,20 @@ def get_model_path():
     model_path = os.path.join(my_path, "model.pkl")
     return model_path
 
-def run_prediction(l:float, w:float):
-    
+def run_prediction(l: float, w: float):
     model_path = get_model_path()
 
     with open(model_path, "rb") as f:
         fish_model = pickle.load(f)
-
+        
     p = fish_model.predict([[l, w]])
 
     if p[0] == 1:
         fish = '도미'
     else:
         fish = '빙어'
-    
+
     print(fish)
 
     return fish
+
